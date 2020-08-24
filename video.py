dbacf15ae6ec5e7f86eb092f069ba13822cd80e2
Pedro Tortello <47224987+PTortello@users.noreply.github.com>
2020-08-24 08:24:33 -0300

    Add files via upload

--- video.py
+++ video.py
@@ -1,13 +1,23 @@
+"""
+Pedro Tortello - 24/08/2020
+IBM Behind the code marathon - Challenge #3
+
+Retrieves data from given urls and saves into JSON files.
+For "body" content check TXT file.
+"""
+
 from bs4 import BeautifulSoup
 from requests import get
 from json import dump
 
 
 def jsoner(address):
+    # Webscraping address argument
     req = get(address).text
     soup = BeautifulSoup(req, 'lxml')
     data = soup.title.text
 
+    # Putting data into dictionary
     dataDict = {}
     dataDict["title"] = data[data.find(':')+2:data.find('|')-1]
     dataDict["author"] = data[:data.find(':')]
@@ -15,11 +25,13 @@ def jsoner(address):
     dataDict["type"] = "video"      # hard coded type !!
     dataDict["url"] = address
 
+    # Creating JSON file
     filename = dataDict["author"].replace(" ", "") + "_video.json"  # !!
     with open(filename, "w", encoding="utf-8") as file:
         dump(dataDict, file, indent=4, ensure_ascii=False)
 
 
+# Given urls list
 addresses = [
 "https://www.ted.com/talks/helen_czerski_the_fascinating_physics_of_everyday_life/transcript?language=pt-br#t-81674",
 "https://www.ted.com/talks/kevin_kelly_how_ai_can_bring_on_a_second_industrial_revolution/transcript?language=pt-br",
@@ -30,5 +42,6 @@ addresses = [
 "https://www.ted.com/talks/tom_gruber_how_ai_can_enhance_our_memory_work_and_social_lives/transcript?language=pt-br"
 ]
 
+# Main loop
 for address in addresses:
     jsoner(address)
